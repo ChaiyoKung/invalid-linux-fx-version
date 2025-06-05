e0d7e4adf9a0e7fc8264e332146de9dcba4a9747
ChaiyoKung <45798076+ChaiyoKung@users.noreply.github.com>
2025-06-06 00:47:45 +0700

    refactor: migrate `azurerm_function_app` to `azurerm_linux_function_app` for fix deprecated (#2)

--- main.tf
+++ main.tf
@@ -38,16 +38,22 @@ resource "azurerm_app_service_plan" "example" {
   }
 }
 
-resource "azurerm_function_app" "example" {
+resource "azurerm_linux_function_app" "example" {
   name                       = "func-linux-fx-version"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
+  service_plan_id            = azurerm_app_service_plan.example.id
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  version                    = "~4"
+  client_certificate_mode    = "Required"
 
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "dotnet"
+  site_config {
+    ftps_state        = "FtpsOnly"
+    use_32_bit_worker = true
+
+    application_stack {
+      use_dotnet_isolated_runtime = true
+      dotnet_version              = "8.0"
+    }
   }
 }
